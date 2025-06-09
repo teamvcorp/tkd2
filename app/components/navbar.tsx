c0daa5a86fb05b561c394b1151c60a1711a9ad5c
Robert Von Der Becke <teamvcorp@thevacorp.com>
2025-06-09 12:11:16 -0500

    correct size issue on mobile

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
@@ -79,17 +79,15 @@ export default function Navbar() {
                             </Link>
                         </div>
                     </div>
-                    <div className="flex items-center">
+                    <div className="hidden md:flex items-center">
                         <div className="shrink-0">
                             <Link
                                 href="/plans"
                                 className="relative inline-flex items-center gap-x-1.5 rounded-md bg-red-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                          
-                               Sign Up Today!
+                                Sign Up Today!
                             </Link>
                         </div>
-                
                     </div>
                 </div>
             </div>
@@ -99,80 +97,41 @@ export default function Navbar() {
                     {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
                     <DisclosureButton
                         as="a"
-                        href="#"
+                        href="/"
                         className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
                     >
                         Home
                     </DisclosureButton>
                     <DisclosureButton
                         as="a"
-                        href="#"
+                        href="/about"
                         className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
                     >
-                        Cl
+                        About
                     </DisclosureButton>
                     <DisclosureButton
                         as="a"
-                        href="#"
+                        href="/classes"
                         className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
                     >
-                        Projects
+                        Classes
                     </DisclosureButton>
                     <DisclosureButton
                         as="a"
-                        href="#"
+                        href="/contact"
                         className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
                     >
-                        Calendar
+                        Contact
+                    </DisclosureButton>
+                    <DisclosureButton
+                        as="a"
+                        href="/members"
+                        className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+                    >
+                        Member
                     </DisclosureButton>
                 </div>
-                <div className="border-t border-gray-200 pb-3 pt-4">
-                    <div className="flex items-center px-4 sm:px-6">
-                        <div className="shrink-0">
-                            <Image
-                                alt=""
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                className="size-10 rounded-full"
-                                unoptimized
-                            />
-                        </div>
-                        <div className="ml-3">
-                            <div className="text-base font-medium text-gray-800">Tom Cook</div>
-                            <div className="text-sm font-medium text-gray-500">tom@example.com</div>
-                        </div>
-                        <button
-                            type="button"
-                            className="relative ml-auto shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">View notifications</span>
-                            <BellIcon aria-hidden="true" className="size-6" />
-                        </button>
-                    </div>
-                    <div className="mt-3 space-y-1">
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                        >
-                            Your Profile
-                        </DisclosureButton>
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                        >
-                            Settings
-                        </DisclosureButton>
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                        >
-                            Sign out
-                        </DisclosureButton>
-                    </div>
-                </div>
+               
             </DisclosurePanel>
         </Disclosure>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Link from 'next/link'
+import Image from 'next/image';
 import ThreeColumnSection from './components/threeColumn';
 
 export default function Example() {
@@ -10,7 +11,7 @@ export default function Example() {
         <div className="relative isolate overflow-hidden px-6 pt-16 justify-betwee sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
 
           <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-            <h2 className="text-balance font-light text-7xl uppercase font-semibold tracking-tight text-white min-w-[700px] sm:text-7xl">
+            <h2 className="text-balance font-light text-3xl sm:text-5xl md:text-7xl uppercase font-semibold tracking-tight text-white min-w-0 sm:min-w-[700px]">
               Building Discipline & Confidence
             </h2>
 
@@ -27,7 +28,7 @@ export default function Example() {
             </div>
           </div>
           <div className="relative mt-16 lg:mt-8">
-            <img
+            <Image
               alt="tkd girl"
               src="/tkdgirl.png"
               width={1000}
