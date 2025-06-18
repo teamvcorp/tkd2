975c8ca5ed60cfefa5cb8e8f989279809aadb5d6
Robert Von Der Becke <teamvcorp@thevacorp.com>
2025-06-18 11:25:32 -0500

    fixed footer bg color

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer: React.FC = () => (
-    <footer className="fixed bottom-0 left-0 w-full bg-black text-white py-4 text-center">
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-[121212] text-white py-4 text-center">
         <span>&copy; {new Date().getFullYear()} VA Corp. All rights reserved.</span>
         &nbsp;|&nbsp;
         <a href="/policy" className="underline hover:text-gray-400">
